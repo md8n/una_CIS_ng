d74595241c30721e2b76dad5c07479b885ae3dfa
md8n <md8n@md8n.com>
2016-06-08 10:34:43 +1000

    Fresh commit

---
+++ src/una_CIS_ng/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace una_CIS_ng.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}

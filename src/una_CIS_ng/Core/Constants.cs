08f5a1664c041e00bd48b8d4650b1011078ec57a
md8n <md8n@md8n.com>
2016-06-08 10:59:49 +1000

    Update to Contacts Page

---
+++ src/una_CIS_ng/Core/Constants.cs
@@ -0,0 +1,20 @@
+﻿namespace una_CIS_ng.Core
+{
+  public static class Constants
+  {
+    public const string LagosFullTitle = "Lagos State";
+    public const string LagosAbbr = "Lagos";
+
+    public const string LasimraFullTitle = LagosFullTitle + " Infrastructure and Maintenance Regulatory Agency";
+    public const string LasimraAbbr = "LASIMRA";
+
+    public const string CisFullTitle = "Critical Infrastructure Services Ltd";
+    public const string CisAbbr = "CIS";
+
+    public const string UnaFullTitle = "Utility Network Audit";
+    public const string UnaAbbr = "UNA";
+
+    public const string LunaFullTitle = LagosFullTitle + " " + UnaFullTitle;
+    public const string LunaAbbr = "L" + UnaAbbr;
+  }
+}

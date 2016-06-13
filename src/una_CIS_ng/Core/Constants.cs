94f7df51c1c0369874312df44e41500986251711
md8n <md8n@md8n.com>
2016-06-13 16:07:57 +1000

    Updates to the index page and removal of connection strings from appsettings.json

--- src/una_CIS_ng/Core/Constants.cs
+++ src/una_CIS_ng/Core/Constants.cs
@@ -7,12 +7,15 @@
 
     public const string LasimraFullTitle = LagosFullTitle + " Infrastructure and Maintenance Regulatory Agency";
     public const string LasimraAbbr = "LASIMRA";
+    public const string LasimraUrl = "http://lasimra.org";
 
     public const string CisFullTitle = "Critical Infrastructural Services Ltd";
     public const string CisAbbr = "CIS";
+    public const string CisUrl = "http://cisng.azurewebsites.net/";
 
     public const string UnaFullTitle = "Utility Network Audit";
     public const string UnaAbbr = "UNA";
+    public const string UnaUrl = "http://unacisng.azurewebsites.net";
 
     public const string LunaFullTitle = LagosFullTitle + " " + UnaFullTitle;
     public const string LunaAbbr = "L" + UnaAbbr;
